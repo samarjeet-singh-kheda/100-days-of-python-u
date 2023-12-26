c91e74113b05837cb484f35c2172b92ec8bdf8b6
samarjeet-singh-kheda <samarjeetsingh259@gmail.com>
2023-12-26 05:40:16 +0530

    day 1 done

---
+++ day-1/main.py
@@ -0,0 +1,27 @@
+# print('Day 1 - Python Print Function \nThe function is declared like this: \nprint(\'what to print\')')
+
+#print("Hello "+ input("What is your name?"))
+
+#print(len(input()))
+
+print("a: " + 2)
+
+'''
+
+--> input() function is used to display a prompt and take user input in that function
+
+--> input function always takes input as a string
+
+--> int() & str() is used to convert into integer and string respectively
+
+--> len() function is used to calculate length of a string
+
+--> In python, snake case is preferred
+
+--> "\n" is used for new line
+
+--> In print statement, either alternate " " & ' ' is used or use \' \' (backslash)
+
+--> In python, type coercion does not happen. Thus, even this concatenation --> print("a: " * 2) --> is invalid
+
+'''
